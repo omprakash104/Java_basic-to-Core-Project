cc6fb984a0432425f4935126ad2d005bb5c31ac0
Shailesh248 <shailesh.bhattarai@preplaced.in>
2022-10-19 08:02:06 +0545

    resetting

--- Java Basic2/src/company2/B.java
+++
@@ -1,8 +0,0 @@
-package company2;
-
-public class B extends A {
-    @Override
-    public void showCount() {
-        System.out.println("class B count");
-    }
-}
--- Java Basic2/src/company2/YoungMale.java
+++
@@ -1,5 +0,0 @@
-package company2;
-
-public class YoungMale {
-
-}
